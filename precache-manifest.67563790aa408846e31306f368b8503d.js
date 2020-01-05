94664e43f67a2f34c2356ea106cc7e8e72fad147
Fedor Konovalov <fedykonovalov@gmail.com>
2020-01-05 12:11:53 +0300

    Updates

---
+++ precache-manifest.67563790aa408846e31306f368b8503d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe67e287ccd74bcbc5338d48d8b000a",
+    "url": "/travel-payouts/index.html"
+  },
+  {
+    "revision": "d5ae9b437b5cf5824322",
+    "url": "/travel-payouts/static/css/main.140925d3.chunk.css"
+  },
+  {
+    "revision": "eacff2c1d33b7117c2e1",
+    "url": "/travel-payouts/static/js/2.df316668.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/travel-payouts/static/js/2.df316668.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5ae9b437b5cf5824322",
+    "url": "/travel-payouts/static/js/main.89da8e34.chunk.js"
+  },
+  {
+    "revision": "35794d062ab13ecb3046",
+    "url": "/travel-payouts/static/js/runtime-main.316741f7.js"
+  }
+]);
